3d6bbbf8e89a6099bf2a59c4cc1830210eb9e96f
vctt94 <victor.duarte.oliveira@gmail.com>
2018-12-05 09:09:03 -0200

    do not show date if timestamp do not exist

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -1,4 +1,7 @@
 export const convertTsToStringDate = (timestamp) => {
+  if (!timestamp) {
+    return;
+  }
   const d = new Date(timestamp * 1000);
   const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   const year = d.getFullYear();
