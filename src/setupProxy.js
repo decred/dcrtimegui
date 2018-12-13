f1658fddc2b454ccf2b2671a48a8535276454df5
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-12-13 16:08:11 -0200

    Remove proxy and add dcr testnet host (#9)

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  // ToDO add config file to switch between testnet and mainnet
-  app.use(proxy('/api', { target: 'https://time-testnet.decred.org:59152/', secure: false, pathRewrite: {
-      "/api": ""
-  }}));
-};
