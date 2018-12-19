a8f1ef7fad8ce9f2bc25369fa1e9c8711901802f
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-12-19 11:51:50 -0200

    General UI/UX improvements (#19)
    * Mix of visual enhancements
    
    * minor results modifications
    
    * fix warning

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Message, Table, TableRow, TableHeader, Link } from "cobra-ui";
-import { CardWrapper, CardContent, ResultsTableData } from "./CommonComponents";
+import styled from "styled-components";
+import { Table, TableRow, TableHeader, Link, theme } from "cobra-ui";
+import { ResultsTableData } from "./CommonComponents";
 import {
   SUCCESS,
   FILE_ALREADY_EXISTS,
@@ -22,117 +23,126 @@ const getHumanReadableResult = result => {
 };
 
 export const AuthenticationResult = ({ files }) => (
-  <>
-    <Message style={{ width: "80%" }} type="success" text="Files Uploaded!" />
-    <CardWrapper>
-      <CardContent>
-        <Table
-          style={{
-            tableLayout: "fixed",
-            width: "100%"
-          }}
-        >
-          <TableRow>
-            <TableHeader>File</TableHeader>
-            <TableHeader>Digest</TableHeader>
-            <TableHeader>Result</TableHeader>
-          </TableRow>
-          {files.map((file, i) => (
-            <TableRow key={`file-result-${i}`}>
-              <ResultsTableData
-                onMouseEnter={el => {
-                  el.target.setAttribute("title", file.name);
-                }}
-              >
-                {file.name}
-              </ResultsTableData>
-              <ResultsTableData
-                style={{ fontFamily: "monospace" }}
-                onMouseEnter={el => {
-                  el.target.setAttribute("title", file.digest);
-                }}
-              >
-                {file.digest}
-              </ResultsTableData>
-              <ResultsTableData>
-                {getHumanReadableResult(file.result)}
-              </ResultsTableData>
-            </TableRow>
-          ))}
-        </Table>
-      </CardContent>
-    </CardWrapper>
-  </>
+  <ResultWrapper>
+    <ResultLabel>Uploaded digests</ResultLabel>
+    <Table
+      style={{
+        tableLayout: "fixed",
+        width: "100%"
+      }}
+    >
+      <TableRow>
+        <TableHeader>File</TableHeader>
+        <TableHeader>Digest</TableHeader>
+        <TableHeader>Result</TableHeader>
+      </TableRow>
+      {files.map((file, i) => (
+        <TableRow key={`file-result-${i}`}>
+          <ResultsTableData
+            onMouseEnter={el => {
+              el.target.setAttribute("title", file.name);
+            }}
+          >
+            {file.name}
+          </ResultsTableData>
+          <ResultsTableData
+            style={{ fontFamily: "monospace" }}
+            onMouseEnter={el => {
+              el.target.setAttribute("title", file.digest);
+            }}
+          >
+            {file.digest}
+          </ResultsTableData>
+          <ResultsTableData>
+            {getHumanReadableResult(file.result)}
+          </ResultsTableData>
+        </TableRow>
+      ))}
+    </Table>
+  </ResultWrapper>
 );
 
 export const VerificationResult = ({ files }) => (
-  <>
-    <Message style={{ width: "80%" }} type="success" text="Files Verified!" />
-    <CardWrapper>
-      <CardContent>
-        <Table>
-          <TableRow>
-            <TableHeader>Anchored Date</TableHeader>
-            <TableHeader>File</TableHeader>
-            <TableHeader>Transaction</TableHeader>
-            <TableHeader>Download</TableHeader>
-            <TableHeader>Result</TableHeader>
+  <ResultWrapper>
+    <ResultLabel>Verified digests</ResultLabel>
+    <Table>
+      <TableRow>
+        <TableHeader>Anchored Date</TableHeader>
+        <TableHeader>File</TableHeader>
+        <TableHeader>Transaction</TableHeader>
+        <TableHeader>Download</TableHeader>
+        <TableHeader>Result</TableHeader>
+      </TableRow>
+      {files.map(
+        (
+          {
+            name,
+            digest: {
+              result,
+              chaininformation: { chaintimestamp, transaction }
+            }
+          },
+          i
+        ) => (
+          <TableRow key={`file-result-${i}`}>
+            <ResultsTableData>
+              {chaintimestamp ? convertTsToStringDate(chaintimestamp) : "-"}
+            </ResultsTableData>
+            <ResultsTableData
+              onMouseEnter={el => {
+                el.target.setAttribute("title", name);
+              }}
+            >
+              {name}
+            </ResultsTableData>
+            <ResultsTableData
+              onMouseEnter={el => {
+                el.target.setAttribute("title", transaction);
+              }}
+            >
+              {chaintimestamp && transaction ? (
+                <Link
+                  style={{ fontFamily: "monospace" }}
+                  target="_blank"
+                  href={`https://testnet.dcrdata.org/tx/${transaction}`}
+                >
+                  {transaction}
+                </Link>
+              ) : (
+                "not anchored yet"
+              )}
+            </ResultsTableData>
+            <ResultsTableData>
+              <DownloadFileLink
+                data={formatDigestToDownload({
+                  name,
+                  transaction,
+                  date: convertTsToStringDate(chaintimestamp),
+                  result: files[i]
+                })}
+                filename={"digest.json"}
+              >
+                click here
+              </DownloadFileLink>
+            </ResultsTableData>
+            <ResultsTableData>
+              {getHumanReadableResult(result)}
+            </ResultsTableData>
           </TableRow>
-          {files.map(
-            (
-              {
-                name,
-                digest: {
-                  result,
-                  chaininformation: { chaintimestamp, transaction }
-                }
-              },
-              i
-            ) => (
-              <TableRow key={`file-result-${i}`}>
-                <ResultsTableData>
-                  {chaintimestamp ? convertTsToStringDate(chaintimestamp) : "-"}
-                </ResultsTableData>
-                <ResultsTableData
-                  onMouseEnter={el => {
-                    el.target.setAttribute("title", name);
-                  }}
-                >
-                  {name}
-                </ResultsTableData>
-                <ResultsTableData
-                  onMouseEnter={el => {
-                    el.target.setAttribute("title", transaction);
-                  }}
-                >
-                  {chaintimestamp && transaction ? (
-                    <Link
-                      style={{ fontFamily: "monospace" }}
-                      target="_blank"
-                      href={`https://testnet.dcrdata.org/tx/${transaction}`}
-                    >
-                      {transaction}
-                    </Link>
-                  ) : (
-                    "not anchored yet"
-                  )}
-                </ResultsTableData>
-                <ResultsTableData>
-                  <DownloadFileLink
-                    data={formatDigestToDownload({name, transaction, date:convertTsToStringDate(chaintimestamp), result: files[i]})}
-                    filename={"digest.json"}
-                  >
-                    click here
-                  </DownloadFileLink>
-                </ResultsTableData>
-                <ResultsTableData>
-                  {getHumanReadableResult(result)}
-                </ResultsTableData>
-              </TableRow>
-            )
-          )}
-        </Table>
-      </CardContent>
-    </CardWrapper>
-  </>
+        )
+      )}
+    </Table>
+  </ResultWrapper>
 );
+
+const ResultWrapper = styled.div`
+  padding: 20px 0;
+  text-align: left;
+  width: 100%;
+`;
+
+const ResultLabel = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${theme.darkGrayColor};
+`;
