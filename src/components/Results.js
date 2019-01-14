d7dc6beac4561bcea9ecee07a398f62193ee06ab
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-01-14 14:40:50 -0200

    Standarize download buttons

--- src/components/Results.js
+++ src/components/Results.js
@@ -156,7 +156,7 @@ export const VerificationResult = ({ files }) => (
                   })}
                   filename={"digest.json"}
                 >
-                  click here
+                  <Button small>Download</Button>
                 </DownloadFileLink>
               </ResultsData>
             </div>
