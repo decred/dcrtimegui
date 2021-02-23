1b160c12b3deba3672728840d69f5630733a1278
David Hill <dhill@mindcry.org>
2021-02-23 11:37:23 +0000

    build: bump deps (#119)
    * build: bump deps
    
    * feat: use ThemeProvider instead of useTheme
    
    * chore: bump babel deps and improve pre-commit hooks
    
    * chore: update github workflow
    
    Co-authored-by: Tiago Alves Dulce <tiagoalvesdulce@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { defaultLightTheme, useTheme, Spinner } from "pi-ui";
+import {
+  defaultLightTheme,
+  ThemeProvider,
+  DEFAULT_LIGHT_THEME_NAME,
+  Spinner
+} from "pi-ui";
 import Routes from "./Routes";
 
+const themes = {
+  [DEFAULT_LIGHT_THEME_NAME]: defaultLightTheme
+};
+
 const App = () => {
-  useTheme(defaultLightTheme);
   return (
-    <Router>
-      <Suspense fallback={<Spinner invert />}>
-        <Routes />
-      </Suspense>
-    </Router>
+    <ThemeProvider themes={themes} defaultThemeName={DEFAULT_LIGHT_THEME_NAME}>
+      <Router>
+        <Suspense fallback={<Spinner invert />}>
+          <Routes />
+        </Suspense>
+      </Router>
+    </ThemeProvider>
   );
 };
 
