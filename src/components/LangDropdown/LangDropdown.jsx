fe1d1ce1aaeb1ce7fa89d76f87fb3fd9b3432228
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2023-12-04 21:04:01 +0700

    Update LangDropdown.jsx

--- src/components/LangDropdown/LangDropdown.jsx
+++ src/components/LangDropdown/LangDropdown.jsx
@@ -5,11 +5,12 @@ import { useTranslation } from "react-i18next";
 
 const LangDropdown = ({className}) => {
     const { i18n } = useTranslation();
-    const allLangOptions = ["ENG", "PT"];
+    const allLangOptions = ["ENG", "PT", "ID"];
     const [selectedLang, setSelectedLang] = useState(localStorage.getItem("lang") || "ENG");
     useEffect(() => {
         if (selectedLang === "ENG") i18n.changeLanguage("en");
         if (selectedLang === "PT") i18n.changeLanguage("pt");
+        if (selectedLang === "ID") i18n.changeLanguage("id");
         localStorage.setItem("lang", selectedLang);
     }, [selectedLang, setSelectedLang, i18n]);
     const handleClickOption = (op) => setSelectedLang(op);
